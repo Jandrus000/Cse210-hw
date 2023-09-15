e6c658b493d5181257f390d21e0a8d56065ad0ab
Jandrus000 <144859065+Jandrus000@users.noreply.github.com>
2023-09-14 20:26:27 -0600

    Finished Core requirements for prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percent? ");
+        var gradePercentstring = Console.ReadLine();
+        double gradePercent = double.Parse(gradePercentstring);
+        string letter = "";
+
+        if (gradePercent>=90)
+            letter = "A";
+        else if (gradePercent>=80)
+            letter = "B";
+        else if (gradePercent>=70)
+            letter = "C";
+        else if (gradePercent>=60)
+            letter = "D";
+        else 
+            letter = "F";
+
+        Console.WriteLine(letter);
+        if (gradePercent>=70)
+            Console.WriteLine("Congrats! You Passed!");
+        else
+            Console.WriteLine("Better luck next time... you failed");
     }
 }
