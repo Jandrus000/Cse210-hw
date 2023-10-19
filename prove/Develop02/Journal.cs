bcf2ebf98f91d238067866486660aa421ab77b19
Jandrus000 <jcandrus02@gmail.com>
2023-10-19 14:49:01 -0600

    fixed naming convention and changed journal

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -45,45 +45,7 @@ public class Journal
         }
     }
 
-    //ChooseOptions displays options to the user, the associated method is called depending what the user chooses
-    //if 5 is entered the loop is broken.
-    //Creativity is shown by continuing the loop and displaying an error message even a number is not entered.
-    public void ChooseOptions()
-    {
-        while (true)
-        {
-            Console.WriteLine("\nPlease Select one of the following choices");
-            Console.WriteLine("1. Write");
-            Console.WriteLine("2. Display");
-            Console.WriteLine("3. Load");
-            Console.WriteLine("4. Save");
-            Console.WriteLine("5. Quit");
-            Console.Write("What would you like to do? ");
-
-            string response_string = Console.ReadLine();
-            int response = 0;
-            if(int.TryParse(response_string, out response))
-                response = Convert.ToInt32(response_string);
-        
-            if(response == 1)
-            {
-                Entry entry = new Entry();
-                entry.MakeEntry();
-                AddEntry(entry);
-            }
-            else if (response == 2) 
-                DisplayJournal();
-            else if (response == 3)
-                LoadJournal();
-            else if (response == 4)
-                SaveJournal();
-            else if (response == 5)
-                break;
-            else
-                Console.WriteLine("Please enter valid menu option");
-        }
-    }
-
+    
     //LoadJournal clears all Entries from current List. Then, reads all lines from .txt file (file name provided by user), each line is put into array
     //of strings. Each string is iterated through in array. Each string is put into prospective TimeDate, PromptUsed and NewEntry depending on what order they appear.
     //When "End of Journal" is encountered the loop breaks.
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -7,7 +7,43 @@ class Program
         //New Journal object is made, welcomes user, and call the Chooseoptions method.
         Journal myJournal = new Journal();
         Console.WriteLine("Welcome to the Journal Program!");
-        myJournal.ChooseOptions();
+        //ChooseOptions displays options to the user, the associated method is called depending what the user chooses
+    //if 5 is entered the loop is broken.
+    //Creativity is shown by continuing the loop and displaying an error message even if a number is not entered.
+        while (true)
+        {
+            Console.WriteLine("\nPlease Select one of the following choices");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+
+            string responseString = Console.ReadLine();
+            int response = 0;
+            if(int.TryParse(responseString, out response))
+                response = Convert.ToInt32(responseString);
+        
+            if(response == 1)
+            {
+                Entry entry = new Entry();
+                entry.MakeEntry();
+                myJournal.AddEntry(entry);
+            }
+            else if (response == 2) 
+                myJournal.DisplayJournal();
+            else if (response == 3)
+                myJournal.LoadJournal();
+            else if (response == 4)
+                myJournal.SaveJournal();
+            else if (response == 5)
+                break;
+            else
+                Console.WriteLine("Please enter valid menu option");
+        
+    }
+
 
 
     }
