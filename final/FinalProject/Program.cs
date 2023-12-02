4d01b53ead7ed742c3455f1d0f3532f6ba905323
Jandrus000 <jcandrus02@gmail.com>
2023-12-02 10:58:48 -0700

    added comments

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -9,13 +9,6 @@ class Program
 
     static void Main(string[] args)
     {
-        // Player p1 = new Player("Joe");
-        // Player p2 = new Player("pearl");
-
-        // p1.IncrementWins();
-        // p1.IncrementWins();
-        // p1.SavePlayer();
-        // p2.SavePlayer();
         CardMatch myGame = new CardMatch();
         while(!myGame.GetWin())
         {
